b9b000ab55487670a886b7601a80a9402edc9d36
Deins <dbang@35gmail.com>
2024-01-23 18:58:32 +0300

    development header

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,64 @@
+import { Component, Fragment } from 'react'
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MegnifyinGlassIcon from '../icon/MegnifyinGlassIcon.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+    <Fragment>
+      <header className='flex-block'>
+        <nav className='header-navigate'>
+          <ul>
+            <li className='header-navigate-logo'>
+              <a><div className='circle'>{ /* temporarily */ }</div></a>
+            </li>
+            <li>
+              <a href='#'>Каталог</a>
+            </li>
+            <li>
+              <a href='#'>Мое обусение</a>
+            </li>
+            <li>
+              <a href='#'>Преподавание</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className='header-profile-wraper flex-block'>
+        {/* --------------- */ }
+          <div className='search-wraper'>
+            <img src={MegnifyinGlassIcon} />
+            <input className='search' placeholder='Поиск...' />
+          </div>
+        {/* --------------- */ }
+          {/* <div className='profile flex-block'>
+              <a href='#'>
+                <p>NameUser</p>
+              </a>
+              <a href='#'>
+                <div className='sqrt'></div>
+              </a>
+          </div> */}
+          <div className='profile flex-block'>
+            <a href='#' className='authenticatin-batten'>Регистрация</a>
+            <a href='#' className='authenticatin-batten'>Войти</a>
+          </div>
+        </div>
       </header>
-    </div>
-  );
+      <main>
+      <p>Главная страниа</p>
+      </main>
+      <footer></footer>
+    </Fragment>
+    )
+  }
+
 }
 
 export default App;
