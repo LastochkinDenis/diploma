38858baca5803964eda4aa6b29bec3f726a11589
Deins <dbang@35gmail.com>
2024-02-08 14:36:13 +0300

    add menu progile

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,10 +8,9 @@ import { Link } from 'react-router-dom';
 
 function Header(props) {
 
-    const [isOpen, setOpen] = useState();
+    const [isOpenMenu, setOpenMenu] = useState(false);
+    const [isOpneProfil, setOpenProfil] = useState(false);
     const user = useSelector((state) => state.user).user;
-
-    console.log(user)
 
     const renderUserBlock = () => {
 
@@ -27,8 +26,20 @@ function Header(props) {
                <div className='header__profile'>
                 <a>{user.firstName + " " + user.lastName}</a>
                 <div className='header__profile_picture'>
-                    <div className='sqrt'>
+                    <div className='sqrt' onClick={() => {
+                         setOpenMenu(false);
+                         setOpenProfil(!isOpneProfil);
+                    }}>
                          <p>{user.firstName[0].toUpperCase() + "" + user.lastName[0].toUpperCase()}</p>
+                    </div>
+                    <div className={`header__profile-menu ${isOpneProfil ? 'activeF' : ''}`}>
+                         <nav>
+                              <ul>
+                                   <li><a href='#'>Профиль</a></li>
+                                   <li><a href='#'>Настройки</a></li>
+                                   <li><a href='#'>Выйти</a></li>
+                              </ul>
+                         </nav>
                     </div>
                 </div>
            </div>
@@ -41,10 +52,13 @@ function Header(props) {
                 <div className='circle'></div>
            </div>
            <button className='header__menu-button'
-           onClick={() => setOpen(!isOpen)}>
+           onClick={() => {
+               setOpenMenu(!isOpenMenu);
+               setOpenProfil(false);
+           }}>
                 <img src={burgerMenuIcon} />
            </button>
-           <nav className={`header__nav ${isOpen  ? " active" : ""}`}>
+           <nav className={`header__nav ${isOpenMenu  ? " active" : ""}`}>
                <ul className={"header__nav-list"}>
                     <li className='header__nav-item'><a href='#'>Каталог</a></li>
                     <li className='header__nav-item'><a href='#'>Мое обучение</a></li>
