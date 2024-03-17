7dacdb44fcdbd60f8ddde0ae8da5028f7512cfcf
Deins <dbang@35gmail.com>
2024-03-17 16:09:02 +0300

    course deshboard list added

--- bekend/app/course/urls.py
+++ bekend/app/course/urls.py
@@ -1,10 +1,10 @@
 from .views import (CreateCourseApi, getInfoCourseApi, UpdateCorseApi,
-                    AddAuthCourse, AddTagsCourse, GetCorseListApi)
+                    AddAuthCourse, AddTagsCourse, GetCourseListApi)
 from django.urls import path
 
 
 urlpatterns = [
-    path('courselist/', GetCorseListApi),
+    path('courselist/', GetCourseListApi),
     path('cratecourse/', CreateCourseApi.as_view()),
     path('<slug:slug>/', getInfoCourseApi),
     path('<slug:slug>/update/', UpdateCorseApi.as_view()),
